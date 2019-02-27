f8e50915f78eb4d75258c06c945aba8cfceb72cc
swChen950415 <359926203@qq.com>
2019-02-27 02:36:29 -0800

    京东爬虫

---
+++ items.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class JdItem(scrapy.Item):
+    # define the fields for your item here like:
+    title = scrapy.Field()  # 标题
+
+    price = scrapy.Field()  # 价格
+
+    comment_num = scrapy.Field()  # 评价条数
+
+    url = scrapy.Field()  # 商品链接
+
+    info = scrapy.Field()  # 详细信息
